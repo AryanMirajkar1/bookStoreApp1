47d775d00fa4d3ce253c9926cd2a4858859f6d10
AryanMirajkar1 <aryanmirajkar70@gmail.com>
2024-07-18 22:30:42 +0530

    Frontend Over !!!

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+import Freebook from './components/Freebook';
+import Home from './home/Home';
+import { Route, Routes } from "react-router-dom"
+import Courses from './courses/Courses';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <>
+    {/* <Home/>
+    <Course/>
+    */}
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/course" element={<Courses/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
